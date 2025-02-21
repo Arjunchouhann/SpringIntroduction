398042efbb0cdc1f0ffb7d1f8cc522677821e8ea
Arjunchouhann <chouhanarjun05112003@gmail.com>
2025-02-21 23:01:55 +0530

    [Arjun] Add.greeting message by rest call

--- src/main/java/com/spring_intro/greeting/controller/UserController.java
+++ src/main/java/com/spring_intro/greeting/controller/UserController.java
@@ -1,13 +1,14 @@
 package com.spring_intro.greeting.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // This annotation marks the class as a REST controller, meaning it will handle HTTP requests
 @RestController
+@RequestMapping("/api")
 public class UserController {
 
-    // Handles GET requests for the "/hello" endpoint
     @GetMapping("/hello")
     public String sayHello() {
         // Returns a simple greeting message as the HTTP response
