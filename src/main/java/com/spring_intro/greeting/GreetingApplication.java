19366e7bfa4620c4e541eea14df1b88c03e72aa7
Arjunchouhann <chouhanarjun05112003@gmail.com>
2025-02-22 10:40:35 +0530

    [Arjun] Add.logger

--- src/main/java/com/spring_intro/greeting/GreetingApplication.java
+++ src/main/java/com/spring_intro/greeting/GreetingApplication.java
@@ -1,13 +1,19 @@
 package com.spring_intro.greeting;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GreetingApplication {
+	//create a logger object
+	private static final Logger logger = LoggerFactory.getLogger(GreetingApplication.class);
 
 	public static void main(String[] args) {
+		logger.info("Spring Boot application is starting...");
 		SpringApplication.run(GreetingApplication.class, args);
+		logger.info("Spring Boot application started successfully");
 	}
 
 }
--- src/main/java/com/spring_intro/greeting/controller/UserController.java
+++ src/main/java/com/spring_intro/greeting/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.spring_intro.greeting.controller;
 
 import com.spring_intro.greeting.bean.Employee;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,5 +34,18 @@ public class UserController {
         return "Employee Id : "+employee.getId() +" , "
                 +"Employee Name : "+employee.getEmployeeName()+ " , "
                 +"Department Name : "+employee.getDepartmentName();
+    }
+
+    //creating a logger instance for this class
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+    //method to log different levels of messages when "/api/log" is accessed
+    @GetMapping("/log")
+    public String logMessages() {
+        logger.info("Information of logMessages() method");
+        logger.debug("Debugging log");
+        logger.warn("warning");
+        logger.error("ERROR");
+        //returning a response to indicate logs have been recorded
+        return "Logs have been recorded.";
     }
 }
